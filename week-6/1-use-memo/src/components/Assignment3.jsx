911c1b53bb6bcf2189d55249c4ebad3f5c584c90
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2024-06-12 10:21:06 +0530

    useMemo assignment

--- week-6/1-use-memo/src/components/Assignment3.jsx
+++ week-6/1-use-memo/src/components/Assignment3.jsx
@@ -7,15 +7,50 @@ export const Assignment3 = () => {
         { name: 'Chocolates', value: 10 },
         { name: 'Chips', value: 20 },
         { name: 'Onion', value: 30 },
-        { name: 'Tomato', value: 30 },
+        { name: 'Tomato', value: 30 }
         // Add more items as needed
     ]);
 
+    const [name, setName] = useState('')
+    const [value, setValue] = useState(0)
+
     // Your code starts here
-    const totalValue = 0;
+    const totalValue = useMemo(() => {
+        let totalVal = 0
+
+        items.forEach(e => {
+            totalVal += Number(e.value)
+        })
+
+        return totalVal
+    }, [items]);
     // Your code ends here
     return (
         <div>
+            <p>name : </p>
+            <input type='text' 
+                    placeholder='name'
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+            ></input>
+            <br />
+            <p>value: </p>
+            <input type='number' 
+                    placeholder='value'
+                    value={value}
+                    onChange={(e) => Number(setValue(e.target.value))}
+            ></input>
+            <br />
+            <button onClick={() => {
+                let newItem = {
+                    name: name,
+                    value: value
+                }
+
+                setItems(items.concat(newItem))
+            }}>
+                add
+            </button>
             <ul>
                 {items.map((item, index) => (
                     <li key={index}>{item.name} - Price: ${item.value}</li>
